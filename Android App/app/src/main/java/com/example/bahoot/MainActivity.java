29208303770fba1e4feef6c57401439ebbad1ae7
Hsin Kai <45195644+Hsinkai2000@users.noreply.github.com>
2023-03-23 09:39:10 +0800

    adding sample execute

--- Android App/app/src/main/java/com/example/bahoot/MainActivity.java
+++ Android App/app/src/main/java/com/example/bahoot/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.bahoot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSend = findViewById(R.id.btnSend);
+    }
+
+    public void linktoXX(View v) {
+        Log.d("TEST", "ALKSDALKSDJ");
+        new HttpTask().execute("http://10.0.0.2:9999/oldegg/signup?email=honda@gmail.com&password=passwordong&name=Honda Ong&confirm=passwordong&mobile=83849233"); // Send HTTP request
+
+    }
+
+    private class HttpTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strURLs) {
+            URL url = null;
+            HttpURLConnection conn = null;
+            try {
+                url = new URL(strURLs[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                // Get the HTTP response code (e.g., 200 for "OK", 404 for "Not found")
+                // and pass a string description in result to onPostExecute(String result)
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {  // 200
+                    return "OK (" + responseCode + ")";
+                } else {
+                    return "Fail (" + responseCode + ")";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // Displays the result of the AsyncTask.
+        // The String result is passed from doInBackground().
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("TAG", "onPostExecute: " + result.toString());
+        }
     }
 }
